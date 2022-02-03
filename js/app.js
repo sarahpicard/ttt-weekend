08709494f6dc3895c0d74a6b33e7fdf4f8ffe609
sarahpicard <sarahannrey225@gmail.com>
2022-02-03 16:24:45 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -3,8 +3,15 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+let isWinner, gameTied, stillPlay, playerTurn
+let boardSquares = []
 
+// 2) Store cached element references on the page that will be accessed in code more than once in variables to make code more concise, readable, and performant:
+	
+	// 2.1) Store the 9 elements that represent the squares on the page.
+	  // You may want to give each square a class name in your HTML to make this easier!
 
+	// 2.2) Store the element that displays the game status on the page.
 
 /*------------------------ Cached Element References ------------------------*/
 
