88a20bbb876e98bbd9b41965b90bab2d290c0ca6
sarahpicard <sarahannrey225@gmail.com>
2022-02-03 16:53:37 -0500

    begin init function

--- js/app.js
+++ js/app.js
@@ -6,13 +6,6 @@
 let isWinner, gameTied, stillPlay, playerTurn
 let boardSquares = []
 
-// 2) Store cached element references on the page that will be accessed in code more than once in variables to make code more concise, readable, and performant:
-	
-	// 2.1) Store the 9 elements that represent the squares on the page.
-	  // You may want to give each square a class name in your HTML to make this easier!
-
-	// 2.2) Store the element that displays the game status on the page.
-
 /*------------------------ Cached Element References ------------------------*/
 const message = document.querySelector('#message')
 const messageDiv = document.querySelector('#message-div')
@@ -23,6 +16,29 @@ const squares = document.querySelector('#board-square')
 /*----------------------------- Event Listeners -----------------------------*/
 
 
+// 3) Upon loading, the app should:
+
+	//// 3.1) Call an initialize function
+
+	// 3.2) That initialize function should initialize the state variables:
+	 // // 3.2.1) Initialize the board array to 9 nulls to represent empty squares. 
+	    // The 9 elements will "map" to each square.
+	    // Index 0 represents the top-left square.
+	    // Index 1 represents the top-middle square.
+			// So on, continuing through the entire board until...
+	    // Index 8 maps to the bottom-right square.
+	  // 3.2.2) Initialize whose turn it is to 1 (player 'X'). 
+	    // Player 'O' will be represented by -1.
+	  // 3.2.3) Initialize the winner variable to null.
+	    // This represents that there is no winner or tie yet. 
+	    // The winner variable will hold the player value (1 or -1) if there's a winner. 
+	    // The winner will hold a 'T' if there's a tie.
+	  // 3.2.4) Render those state variables to the page by calling a render function.
+
 
 /*-------------------------------- Functions --------------------------------*/
+init ()
 
+function init() {
+  squares.textContent = ''
+}
