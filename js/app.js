ea6661dfd2fa869c826e9b74fef4bca65f1b0fb2
sarahpicard <sarahannrey225@gmail.com>
2022-02-04 16:44:34 -0500

    complete render function

--- js/app.js
+++ js/app.js
@@ -3,59 +3,77 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let isWinner, gameTied, stillPlay, playerTurn
-let playerX = 1
-let playerO = -1
-let boardSquares = []
+let isWinner, board, playerTurn
+
 
 /*------------------------ Cached Element References ------------------------*/
 const message = document.querySelector('#message')
-const messageDiv = document.querySelector('#message-div')
+// const messageDiv = document.querySelector('#message-div')
 const gameBoard = document.querySelector('#board')
+
 const squares = document.querySelector('.board-square')
-const squareZero = document.querySelector('#sq0')
-const squareOne = document.querySelector('#sq1')
-const squareTwo = document.querySelector('#sq2')
-const squareThree = document.querySelector('#sq3')
-const squareFour = document.querySelector('#sq4')
-const squareFive = document.querySelector('#sq5')
-const squareSix = document.querySelector('#sq6')
-const squareSeven = document.querySelector('#sq7')
-const squareEight = document.querySelector('#sq8')
+
+// const squareZero = document.querySelector('#sq0')
+// const squareOne = document.querySelector('#sq1')
+// const squareTwo = document.querySelector('#sq2')
+// const squareThree = document.querySelector('#sq3')
+// const squareFour = document.querySelector('#sq4')
+// const squareFive = document.querySelector('#sq5')
+// const squareSix = document.querySelector('#sq6')
+// const squareSeven = document.querySelector('#sq7')
+// const squareEight = document.querySelector('#sq8')
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-squares.addEventListener('click', render)
+squares.forEach((square) => {
+	square.addEventListener('click', handleClick)
+})
 
 
 /*-------------------------------- Functions --------------------------------*/
 init()
 
-
 function init() {
   message.textContent = 'Player X, please choose a square'
-  squares.textContent = ''
   playerTurn = 1
   isWinner = null
-  // playerO = -1
-  // playerX = 1
+	boardSquares = [null, null, null, null, null, null, null, null, null]
   render()
 }
-//player 1 and Player 0(rep w/ -1)
 //isWinner variable: T (tie), 1 (player 1 wins), -1 (player 0 wins)
 
-function render() {
-  for (let i = 0; i < boardSquares.length; i++) {
+console.log(boardSquares)
 
-  }
+function render() {
+	boardSquares.forEach((cell, idx) => {
+		let cellLetter
+		if (board[idx] === 1) {
+			cellLetter = 'X'
+		} else if (board[idx] === -1) {
+			cellLetter = 'O'
+		} else if (board[idx] === null) {
+			cellLetter = ''
+		} 
+		cell.innerText = cellLetter
+	})
+
+	if (!isWinner) {
+		message.innerText = `It's ${turn === 1 ? 'X' : '0'}'s turn!`
+	} else if (isWinner === 'T') {
+		message.innerText = `It's a tie!`
+	} else {
+		message.innerText = `Congratualtions ${isWinner === 1 ? 'X' : 'O'}!`
+	}
 }
+
 
 // 3.3) The render function should:
 	  // 3.3.1) Loop over the board array (which represents the squares on the page), and for each iteration:
 		  // 3.3.1.1) Use the index of the iteration to access the square in the squares array that corresponds with the current cell being iterated over in the board array
 		  // 3.3.1.2) Style that square however you wish dependant on the value contained in the current cell being iterated over (-1, 1, or null)
+
 	  // 3.3.2) Render a message reflecting the currrent game state:
 	    // 3.3.2.1) If winner has a value other than null (game still in progress), render whose turn it is.
 	      // Hint: Maybe use a ternary inside of a template literal here?
